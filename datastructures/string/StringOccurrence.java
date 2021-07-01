66131da293cd0874b9c18e286277f130344b865e
mythirdlanguage <85183938+mythirdlanguage@users.noreply.github.com>
2021-07-01 08:57:33 +0530

    String Occurence

--- datastructures/string/StringOccurrence.java
+++ datastructures/string/StringOccurrence.java
@@ -4,7 +4,7 @@ package string;
     Task is to implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
     Note: You are not allowed to use inbuilt function.
    
-   	Input:
+   	Input: 
 		s = GeeksForGeeks, x = For
 	Output: 5
 	Explanation: For is present as substring in GeeksForGeeks from index 5 (0 based indexing).
